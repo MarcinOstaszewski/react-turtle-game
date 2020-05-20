aeae3a1acc75334f25b56989a6b335c511bb41e8
MarcinOstaszewski <marcinostaszewski@gmail.com>
2020-05-20 10:56:58 +0200

    changed window size handling

--- src/App.js
+++ src/App.js
@@ -1,58 +1,66 @@
-import React, {Component} from 'react';
-// import './App.css';
+import React, { Component } from 'react';
+import './App.css';
 import Scene from './containers/Scene/Scene';
 import Tortoise from './containers/Tortoise/Tortoise';
 
 class App extends Component {
 
-  state= {
-    left: '200px',
-    top: '590px',
+  state = {
+    windowInnerWidth: window.innerWidth,
+    windowInnerHeight: window.innerHeight,
+    left: '600px',
+    top: '400px',
     rotation: 0,
     keysPressed: {},
+    tortoise: {
+      left: 0,
+      top: 0, 
+    },
   }
+  frameLength = 16;
 
-  changeScreenSize = () => {
+  checkWindowSize = () => {
     this.setState({
-      windowInnerWidth: window.innerWidth + 'px',
-      windowInnerHeight: window.innerHeight + 'px'
+      windowInnerWidth: window.innerWidth,
+      windowInnerHeight: window.innerHeight
     })
   }
+
   componentDidMount() {
     document.onkeyup = (e) => {
       const keys = {...this.state.keysPressed};
       delete keys[e.key];
       this.setState({ keysPressed: keys })
     }
-    document.onkeydown = (e) =>  {
+    document.onkeydown = (e) => {
       const keys = {
         ...this.state.keysPressed,
-        [e.key] : 1
+        [e.key]: 1
       }
       this.setState({ keysPressed: keys })
     }
-    
-    window.onresize = () => this.changeScreenSize();
-    this.changeScreenSize();
+    window.onresize = () => this.checkWindowSize();
+    this.checkWindowSize();
   }
 
   render() {
-    return (
+    return ( 
       <div>
-        <Scene width={this.state.windowInnerWidth} 
-          height={this.state.windowInnerHeight} 
-          backgroundColor="#444" />
-        <Tortoise 
-          width={this.state.windowInnerWidth} 
-          height={this.state.windowInnerHeight}
-          left={this.state.left} 
+        <Scene width={this.state.windowInnerWidth + 'px'}
+          height={this.state.windowInnerHeight + 'px'}
+          backgroundColor="#444" 
+        />
+        <Tortoise scrWidth={this.state.windowInnerWidth}
+          scrHeight={this.state.windowInnerHeight}
+          left={this.state.left}
           top={this.state.top}
           rotation={this.state.rotation}
           keysPressed={this.state.keysPressed}
+          frameLength={this.frameLength}
         />
       </div>
-    );
-  }
+  );
+}
 }
 
 export default App;
