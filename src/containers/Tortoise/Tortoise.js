b28146481c4ed4a3ae24fc392dcce1ca8bb23aba
MarcinOstaszewski <marcinostaszewski@gmail.com>
2020-05-20 11:45:21 +0200

    refactoring, moved funcs to helper module

--- src/containers/Tortoise/Tortoise.js
+++ src/containers/Tortoise/Tortoise.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
 import styles from './Tortoise.module.css'
+import {
+    setPlayerPosition,
+    setPlayerRotation,
+    verifyBounce,
+    checkCollisions,
+    moveStar,
+    calculateVelocityAndRotation,
+    flapsMoving,
+    consts
+} from '../../helperFunctions';
 
 class Tortoise extends Component {
     state = {
@@ -38,92 +48,7 @@ class Tortoise extends Component {
         head: '-10px',
         rotationVelocity: 0,
     }
-    starColors = ['#C5DE79', '#C0DA74', '#ADC668', '#9AB15D', '#869D51', '#738846', '#60743A', '#4D602E', '#3A4B23', '#263717']
     starInterval;
-    maxStarsCount = 1;
-    bounceFactor = -0.33;
-    maxVelocity = 0.25;
-    maxRotation = 2.5;
-    size = 40;
-
-    flapsMoving = (obj) => {
-        let flap = Object.assign({}, {...obj})
-        if (flap.moving === 0) {
-            if (this.props.keysPressed[flap.key]) {
-                return {
-                    transform: 'rotate(' + (flap.sign ? '-' : '') + '20deg)',
-                    moving: 60,
-                    speed: 1.3
-                }
-            } else return {}
-        } else if (flap.moving === 30) { // flap starts to return
-            return {
-                transform: 'rotate(' + (flap.sign ? '-' : '') + '80deg)',
-                moving: --flap.moving,
-                speed: 0
-            }
-        } else if (flap.moving <= 60 && flap.moving > 0) {
-            return {
-                moving: --flap.moving,
-                speed: (flap.moving > 55)  ?  flap.speed * 1.15  :  flap.speed * 0.97,
-            }
-        }
-    }
-
-    calculateVelocityAndRotation(obj) {
-        let values = Object.assign({}, {...obj})
-        if (Math.abs(values.verticalVelocity) > 0.01) {
-            values.verticalVelocity = values.verticalVelocity * 0.97; // gradually slows down speed
-        } else {
-            values.verticalVelocity = 0; // stops
-        }
-        if (Math.abs(values.horizontalVelocity) > 0.01) {
-            values.horizontalVelocity = values.horizontalVelocity * 0.97;
-        } else {
-            values.horizontalVelocity = 0;
-        }
-        if (values.rotationVelocity > this.maxRotation) { 
-            values.rotationVelocity = this.maxRotation; // rotation cannot exceed max...
-        } else if (values.rotationVelocity < -this.maxRotation) { 
-            values.rotationVelocity = -this.maxRotation; // ... nor -max
-        } else {
-            if (Math.abs(values.rotationVelocity) > 0.1) {
-                values.rotationVelocity *= 0.97; // slowing down rotation
-            } else {
-                values.rotationVelocity = 0;
-            }
-        }
-        return {
-            rotation: values.rotation,
-            verticalVelocity: values.verticalVelocity,
-            horizontalVelocity: values.horizontalVelocity
-        }
-    }
-
-    verifyBounce = (left, top, hVelocity, vVelocity, size, width, height, bounceFactor) => {
-        let values = {
-            horizontalVelocity: hVelocity,
-            verticalVelocity: vVelocity,
-        }
-        if (top + hVelocity > height - size) { values.horizontalVelocity *= bounceFactor; }
-        if (left + vVelocity > width - size) { values.verticalVelocity *= bounceFactor; }
-        if (top + hVelocity < 0) { values.horizontalVelocity *= bounceFactor; }
-        if (left + vVelocity < 0) { values.verticalVelocity *= bounceFactor; }
-        return values;
-    }
-
-    setPlayerPosition = (left, top, hVelocity, vVelocity) => {
-        return {
-            left: (left + vVelocity) + "px",
-            top: (top + hVelocity) + "px"
-        }
-    }
-
-    setPlayerRotation = (rotation) => {
-        let rot = 0 + (rotation % 360);
-        if (rotation < -180) { rot = rotation + 360 }
-        return rot;
-    }
 
     update = () => {
         let tempVal = {
@@ -140,13 +65,13 @@ class Tortoise extends Component {
         
         if (this.props.keysPressed['w']) { tempVal.head = '-20px'; }
         
-        Object.assign(tempVal.leftFlap, this.flapsMoving(tempVal.leftFlap)) // naspisuje w tempVal zmienne dla płetw
-        Object.assign(tempVal.rightFlap, this.flapsMoving(tempVal.rightFlap))
+        Object.assign(tempVal.leftFlap, flapsMoving(tempVal.leftFlap, this.props.keysPressed)) // overwrite in tempVal variables for flap(s)
+        Object.assign(tempVal.rightFlap, flapsMoving(tempVal.rightFlap, this.props.keysPressed))
         
         tempVal.rotation = tempVal.rotation + tempVal.leftFlap.speed - tempVal.rightFlap.speed;
 
-        tempVal.horizontalVelocity = this.state.horizontalVelocity - (this.maxVelocity * (tempVal.leftFlap.speed + tempVal.rightFlap.speed) / 4) * Math.cos(this.state.rotation * Math.PI / 180);
-        tempVal.verticalVelocity = this.state.verticalVelocity + (this.maxVelocity * (tempVal.leftFlap.speed + tempVal.rightFlap.speed) / 4) * Math.sin(this.state.rotation * Math.PI / 180);
+        tempVal.horizontalVelocity = this.state.horizontalVelocity - (consts.maxVelocity * (tempVal.leftFlap.speed + tempVal.rightFlap.speed) / 4) * Math.cos(this.state.rotation * Math.PI / 180);
+        tempVal.verticalVelocity = this.state.verticalVelocity + (consts.maxVelocity * (tempVal.leftFlap.speed + tempVal.rightFlap.speed) / 4) * Math.sin(this.state.rotation * Math.PI / 180);
         
         if (this.props.keysPressed['s']) {
             tempVal.rearRightTransform = "rotate(-40deg)";
@@ -154,11 +79,11 @@ class Tortoise extends Component {
             tempVal.horizontalVelocity = 0.97 * tempVal.horizontalVelocity;
             tempVal.verticalVelocity = 0.97 * tempVal.verticalVelocity;
         }
-        Object.assign(tempVal, this.calculateVelocityAndRotation(tempVal));
+        Object.assign(tempVal, calculateVelocityAndRotation(tempVal, consts.maxRotation));
 
-        for (let i = 0; i < this.maxStarsCount; i++) {
-            if (this.checkCollisions(
-                this.size, tempVal.left, tempVal.top,  // tortoise
+        for (let i = 0; i < consts.maxStarsCount; i++) {
+            if (checkCollisions(
+                consts.tortoiseSize, tempVal.left, tempVal.top,  // tortoise
                 this.state.starsArr[i] // star
             )) {
                 window.clearInterval(this.starInterval);
@@ -166,28 +91,18 @@ class Tortoise extends Component {
             }
         }
 
-        Object.assign(tempVal, this.verifyBounce(tempVal.left, tempVal.top, tempVal.horizontalVelocity, tempVal.verticalVelocity, this.size, this.state.scrWidth, this.state.scrHeight, this.bounceFactor));
-        Object.assign(tempVal, this.setPlayerPosition(tempVal.left, tempVal.top, tempVal.horizontalVelocity, tempVal.verticalVelocity));
-        Object.assign(tempVal, this.setPlayerRotation(tempVal.rotation));
+        Object.assign(tempVal, verifyBounce(tempVal.left, tempVal.top, tempVal.horizontalVelocity, tempVal.verticalVelocity, consts.tortoiseSize, this.state.scrWidth, this.state.scrHeight, consts.bounceFactor));
+        Object.assign(tempVal, setPlayerPosition(tempVal.left, tempVal.top, tempVal.horizontalVelocity, tempVal.verticalVelocity));
+        Object.assign(tempVal, setPlayerRotation(tempVal.rotation));
         
         this.setState({...tempVal})
-    }
-
-    checkCollisions = (s1, x1, y1, star) => {
-        let s2 = star.size;
-        let x2 = parseInt(star.left.slice(0, -2));
-        let y2 = parseInt(star.top.slice(0, -2));
-        let dx = x1 - x2;
-        let dy = y1 - y2;
-        let dist = Math.sqrt(dx * dx + dy * dy);
-        return dist < (s1 + s2) * 0.7 ? true : false;
     }
     
     placeStar = (i) => {
         let x = Math.floor(Math.random() * (this.state.scrWidth - 60) + 30);
         let y = Math.floor(Math.random() * (this.state.scrHeight - 60) + 30);
         this.setState(state => {
-            const stars = state.starsArr.map((item,j) => {
+            const stars = state.starsArr.map((item, j) => {
                 if (j === i) {
                     return {
                         ...item,
@@ -203,58 +118,15 @@ class Tortoise extends Component {
 
             return { starsArr: stars }
         })
-        this.starInterval = window.setInterval(this.moveStar.bind(null, 0), this.props.frameLength);
-    }
-            
-    moveStar = (i) => {
-        let left = Number(this.state.starsArr[i].left.slice(0, -2));
-        let top = Number(this.state.starsArr[i].top.slice(0, -2));
-        let hSpeed = this.state.starsArr[i].hSpeed;
-        let vSpeed = this.state.starsArr[i].vSpeed;
-        if (left + this.state.starsArr[i].hSpeed <= 0) {
-            hSpeed = 0 - this.state.starsArr[i].hSpeed;
-        }
-        if (left + this.state.starsArr[i].hSpeed > this.state.scrWidth - this.state.starsArr[i].size) {
-            hSpeed = 0 - this.state.starsArr[i].hSpeed;
-        }
-        if (top + this.state.starsArr[i].vSpeed < 0) {
-            vSpeed = 0 - this.state.starsArr[i].vSpeed;
-        }
-        if (top + this.state.starsArr[i].vSpeed > this.state.scrHeight - this.state.starsArr[i].size) {
-            vSpeed = 0 - this.state.starsArr[i].vSpeed;
-        }
-        this.setState(state => {
-            const stars = state.starsArr.map((item,j) => {
-                if (j === i) {
-                    return {
-                        ...item, // this.state.starsArr[i]
-                        left: (left + hSpeed) + 'px',
-                        top: (top + vSpeed) + 'px',
-                        vSpeed: vSpeed,
-                        hSpeed: hSpeed
-                    }
-                } else {
-                    return item
-                }
-            })
-
-            return { starsArr: stars}
-            // star: {
-            //     ...this.state.starsArr[i],
-            //     left: (left + hSpeed) + 'px',
-            //     top: (top + vSpeed) + 'px',
-            //     vSpeed: vSpeed,
-            //     hSpeed: hSpeed
-            // }
-        })
-    }
+        this.starInterval = window.setInterval(moveStar.bind(null, i, this), this.props.frameLength);
+    }    
 
     componentDidMount() {
         this.interval = window.setInterval(this.update, this.props.frameLength);
     }
     componentDidUpdate(prevProps, prevState) {
         if (this.state.starsArr[0].left === '' && this.state.scrHeight && this.state.scrWidth) {
-            for (let i = 0; i < this.maxStarsCount; i++) {
+            for (let i = 0; i < consts.maxStarsCount; i++) {
                 this.placeStar(i);
             }
         }
@@ -289,7 +161,7 @@ class Tortoise extends Component {
                     style={{
                         left: this.state.starsArr[0].left,
                         top: this.state.starsArr[0].top,
-                        backgroundColor: this.starColors[0]
+                        backgroundColor: consts.starColors[0]
                 }}></div>
             </div>
          );
---
+++ src/helperFunctions.js
@@ -0,0 +1,145 @@
+
+let setPlayerPosition = (left, top, hVelocity, vVelocity) => {
+    return {
+        left: (left + vVelocity) + "px",
+        top: (top + hVelocity) + "px"
+    }
+}
+
+let setPlayerRotation = (rotation) => {
+    let rot = 0 + (rotation % 360);
+    if (rotation < -180) { rot = rotation + 360 }
+    return rot;
+}
+
+let verifyBounce = (left, top, hVelocity, vVelocity, size, width, height, bounceFactor) => {
+    let values = {
+        horizontalVelocity: hVelocity,
+        verticalVelocity: vVelocity,
+    }
+    if (top + hVelocity > height - size) { values.horizontalVelocity *= bounceFactor; }
+    if (left + vVelocity > width - size) { values.verticalVelocity *= bounceFactor; }
+    if (top + hVelocity < 0) { values.horizontalVelocity *= bounceFactor; }
+    if (left + vVelocity < 0) { values.verticalVelocity *= bounceFactor; }
+    return values;
+}
+
+let checkCollisions = (s1, x1, y1, star) => {
+    let s2 = star.size;
+    let x2 = parseInt(star.left.slice(0, -2));
+    let y2 = parseInt(star.top.slice(0, -2));
+    let dx = x1 - x2;
+    let dy = y1 - y2;
+    let dist = Math.sqrt(dx * dx + dy * dy);
+    return dist < (s1 + s2) * 0.7 ? true : false;
+}
+
+let moveStar = (i, that) => {
+    let tortoise = that
+    let left = Number(tortoise.state.starsArr[i].left.slice(0, -2));
+    let top = Number(tortoise.state.starsArr[i].top.slice(0, -2));
+    let hSpeed = tortoise.state.starsArr[i].hSpeed;
+    let vSpeed = tortoise.state.starsArr[i].vSpeed;
+    if (left + tortoise.state.starsArr[i].hSpeed <= 0) {
+        hSpeed = 0 - tortoise.state.starsArr[i].hSpeed;
+    }
+    if (left + tortoise.state.starsArr[i].hSpeed > tortoise.state.scrWidth - tortoise.state.starsArr[i].size) {
+        hSpeed = 0 - tortoise.state.starsArr[i].hSpeed;
+    }
+    if (top + tortoise.state.starsArr[i].vSpeed < 0) {
+        vSpeed = 0 - tortoise.state.starsArr[i].vSpeed;
+    }
+    if (top + tortoise.state.starsArr[i].vSpeed > tortoise.state.scrHeight - tortoise.state.starsArr[i].size) {
+        vSpeed = 0 - tortoise.state.starsArr[i].vSpeed;
+    }
+    tortoise.setState(state => {
+        const stars = state.starsArr.map((item, j) => {
+            if (j === i) {
+                return {
+                    ...item,
+                    left: (left + hSpeed) + 'px',
+                    top: (top + vSpeed) + 'px',
+                    vSpeed: vSpeed,
+                    hSpeed: hSpeed
+                }
+            } else { return item }
+        })
+
+        return { starsArr: stars }
+    })
+}
+
+let calculateVelocityAndRotation = (obj, maxRotation) => {
+    let values = Object.assign({}, {...obj})
+    if (Math.abs(values.verticalVelocity) > 0.01) {
+        values.verticalVelocity = values.verticalVelocity * 0.97; // gradually slows down speed
+    } else {
+        values.verticalVelocity = 0; // stops
+    }
+    if (Math.abs(values.horizontalVelocity) > 0.01) {
+        values.horizontalVelocity = values.horizontalVelocity * 0.97;
+    } else {
+        values.horizontalVelocity = 0;
+    }
+    if (values.rotationVelocity > maxRotation) { 
+        values.rotationVelocity = maxRotation; // rotation cannot exceed max...
+    } else if (values.rotationVelocity < -maxRotation) { 
+        values.rotationVelocity = -maxRotation; // ... nor -max
+    } else {
+        if (Math.abs(values.rotationVelocity) > 0.1) {
+            values.rotationVelocity *= 0.97; // slowing down rotation
+        } else {
+            values.rotationVelocity = 0;
+        }
+    }
+    return {
+        rotation: values.rotation,
+        verticalVelocity: values.verticalVelocity,
+        horizontalVelocity: values.horizontalVelocity
+    }
+}
+
+let flapsMoving = (obj, keysPressed) => {
+    let flap = Object.assign({}, {...obj})
+    if (flap.moving === 0) {
+        if (keysPressed[flap.key]) {
+            return {
+                transform: 'rotate(' + (flap.sign ? '-' : '') + '20deg)',
+                moving: 60,
+                speed: 1.3
+            }
+        } else return {}
+    } else if (flap.moving === 30) { // flap starts to return
+        return {
+            transform: 'rotate(' + (flap.sign ? '-' : '') + '80deg)',
+            moving: --flap.moving,
+            speed: 0
+        }
+    } else if (flap.moving <= 60 && flap.moving > 0) {
+        return {
+            moving: --flap.moving,
+            speed: (flap.moving > 55)  ?  flap.speed * 1.15  :  flap.speed * 0.97,
+        }
+    }
+}
+
+let consts = {
+    starColors: ['#C5DE79', '#C0DA74', '#ADC668', '#9AB15D', '#869D51', '#738846', '#60743A', '#4D602E', '#3A4B23', '#263717'],
+    maxStarsCount: 1,
+    bounceFactor: -0.33,
+    maxVelocity: 0.25,
+    maxRotation: 2.5,
+    tortoiseSize: 40,
+}
+
+
+export {
+    setPlayerPosition, 
+    setPlayerRotation, 
+    verifyBounce,
+    checkCollisions,
+    moveStar,
+    calculateVelocityAndRotation,
+    flapsMoving,
+    consts
+}
