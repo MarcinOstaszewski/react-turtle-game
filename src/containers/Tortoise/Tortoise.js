05b00067109dc25e4e3045aec04bd16029611534
MarcinOstaszewski <marcinostaszewski@gmail.com>
2020-05-30 19:46:04 +0200

    add resetState for gameState, healing, faster with "w", rem star rotation

--- src/containers/Tortoise/Tortoise.js
+++ src/containers/Tortoise/Tortoise.js
@@ -13,52 +13,63 @@ import {
 
 class Tortoise extends Component {
     state = {
-        left: this.props.scrWidth / 2 + 'px', // tortoise horiz. position
-        top: this.props.scrHeight / 2 + 'px', // tortoise vert. position
-        rotation: this.props.rotation || 0,
-        horizontalVelocity: this.props.horizontalVelocity || 0,
-        verticalVelocity: this.props.verticalVelocity || 0,
-        health: this.props.health,
+        starsArr: [],
         leftFlap: {
-            moving: 0,
-            speed: 0,
             transform: '',
-            sign: 0,
-            key: 'a'
         },
         rightFlap: {
-            moving: 0,
-            speed: 0,
             transform: '',
-            sign: 1, 
-            key: 'd'
         },
-        starsArr: [],
-        rearRightTransform: '',
-        rearLeftTransform: '',
         head: '-10px',
         rotationVelocity: 0,
     }
     starInterval = [];
     tmOut;
 
+    resetThisState = () => {
+        this.setState({
+            rearRightTransform: '',
+            rearLeftTransform: '',
+            left: this.props.scrWidth / 2 + 'px', // tortoise horiz. position
+            top: this.props.scrHeight / 2 + 'px', // tortoise vert. position
+            rotation: 0,
+            horizontalVelocity: 0,
+            verticalVelocity: 0,
+            health: this.props.health,
+            leftFlap: {
+                moving: 0,
+                speed: 0,
+                transform: '',
+                sign: 0,
+                key: 'a'
+            },
+            rightFlap: {
+                moving: 0,
+                speed: 0,
+                transform: '',
+                sign: 1,
+                key: 'd'
+            },
+            head: '-10px',
+            rotationVelocity: 0, 
+        })
+    }
+
     update = () => {
-        if (this.props.isGameOn) {
+        if (this.props.gameState === 'game') {
             let tempVal = {
                 leftFlap: {...this.state.leftFlap},
                 rightFlap: {...this.state.rightFlap},
                 rearLeftTransform: "rotate(-20deg)", // starting position
                 rearRightTransform: "rotate(20deg)",
-                top: parseFloat(this.state.top), // Number(this.state.top.slice(0, -2)),
-                left: parseFloat(this.state.left), //Number(this.state.left.slice(0, -2)),
+                top: parseFloat(this.state.top),
+                left: parseFloat(this.state.left),
                 rotation: this.state.rotation + 0,
                 rotationVelocity: this.state.rotationVelocity,
                 head: '-10px',
                 health: this.state.health
             }
-            
-            if (this.props.keysPressed['w']) { tempVal.head = '-20px'; }
-            
+
             Object.assign(tempVal.leftFlap, flapsMoving(tempVal.leftFlap, this.props.keysPressed)) // overwrite in tempVal variables for flap(s)
             Object.assign(tempVal.rightFlap, flapsMoving(tempVal.rightFlap, this.props.keysPressed))
             
@@ -81,34 +92,63 @@ class Tortoise extends Component {
                     consts.tortoiseSize, tempVal.left, tempVal.top,  // tortoise
                     star // star
                 )) {
-                    let score = parseInt(Math.abs(tempVal.horizontalVelocity) + Math.abs(tempVal.verticalVelocity) + Math.abs(star.hSpeed) + Math.abs(star.vSpeed));
-                    this.props.addToScore(score);
+                    let score = parseInt(Math.abs(tempVal.horizontalVelocity) + Math.abs(tempVal.verticalVelocity) + Math.abs(star.hSpeed) + Math.abs(star.vSpeed)) + (consts.starColors.length - star.bgColor);
+                    if (star.bgColor < 3) {
+                        console.log('<3')
+                        if (tempVal.health < 100) {
+                            console.log('health < 100')
+                            tempVal.health -= 4 * star.bgColor - 12; // heals 
+                        } else {
+                            console.log('MULTI')
+                            this.props.addToScore(score * 2); // OR multiples score when 100% healthy
+                        }
+                    } else {
+                        this.props.addToScore(score); // OR just scores
+                    }
                     window.clearInterval(this.starInterval[i]);
                     placeStar(i, this)
                 }
+            }
+
+            if (this.props.keysPressed['w']) { 
+                tempVal.head = '-20px'; 
+                tempVal.horizontalVelocity *= 1.005;
+                tempVal.verticalVelocity *= 1.005;
             }
 
             Object.assign(tempVal, verifyBounce(tempVal, consts, this.props));
             Object.assign(tempVal, setPlayerPosition(tempVal.left, tempVal.top, tempVal.horizontalVelocity, tempVal.verticalVelocity));
             Object.assign(tempVal, setPlayerRotation(tempVal.rotation));
 
-            let healthChange = this.state.health - tempVal.health
+            let healthChange;
+            if (this.state.health) {
+                healthChange = this.state.health - tempVal.health
+            }
 
             this.setState({...tempVal})
-            
-            if (healthChange !== 0) {
+            if (healthChange && healthChange !== 0) {
                 this.props.updateHealth(healthChange)
             }
         }
     }
 
     componentDidMount() {
+        this.resetThisState();
         window.onresize = () => this.props.checkWindowSize();
         this.interval = window.setInterval(this.update, this.props.frameLength);
     }
     componentDidUpdate(prevProps, prevState) {
-        if (this.state.starsArr.length < consts.maxStarsCount) { 
-            // this.tmOut = window.setTimeout(placeStar.bind(null, this.state.starsArr.length, this), getRandomNumBetween(500, 1500))
+        if (prevProps.gameState === 'start' && this.props.gameState === 'game') {
+            console.log(this.props.left, this.props.top)
+            this.setState({
+                rotation: 0,
+                left: this.props.scrWidth / 2 + 'px', 
+                top: this.props.scrHeight / 2 + 'px',
+                horizontalVelocity: 0,
+                verticalVelocity: 0,
+            })
+        }
+        if (this.state.starsArr.length < consts.maxStarsCount) {
             placeStar(this.state.starsArr.length, this);
         }
     }
@@ -124,9 +164,9 @@ class Tortoise extends Component {
                     style={{
                         left: item.left,
                         top: item.top,
-                        backgroundColor: item.bgColor,
-                        transform: `rotate(${item.transform}deg)`
+                        backgroundColor: consts.starColors[item.bgColor],
                     }}>
+                    
                 </div>
             )
         })
