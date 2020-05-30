eaa6dcdc6162f630cb1afac27fbf20f873397614
MarcinOstaszewski <marcinostaszewski@gmail.com>
2020-05-30 19:41:47 +0200

    rem Routing, chgd Start info

--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -2,15 +2,17 @@ import React from 'react';
 import styles from './Start.module.css'
 
 const Start = (props) => {
-    // console.log(props)
+    
     return ( 
         <div id={styles.start}>
-            <p>Your goal is to catch the stars.</p>
-            <p>The greater the speed the more points you score.</p>
-            <p>Avoid collisions with the walls.</p>
-            <p>Use w, s, a, d keys.</p>
+            <div>Catch the circles for points.</div>
+            <div>Brighter circles give more points.</div>
+            <div>The greater is your's and circle's speed the more you score.</div>
+            <div>Collisions at high speed hurt you.</div>
+            <div>Red circles heal you.</div>
+            <div>Use <strong>w, s, a, d</strong> keys.</div>
             <div className={styles.btn}
-                onClick={()=> props.history.push('/gameon')}
+                onClick={ () => props.changeGameState('game') }
             >START</div>
 
         </div>
