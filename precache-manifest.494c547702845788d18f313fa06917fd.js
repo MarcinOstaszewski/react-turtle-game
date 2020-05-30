710b13c9b20631b445530652a9223422bba76e75
MarcinOstaszewski <marcinostaszewski@gmail.com>
2020-05-30 19:51:36 +0200

    Updates

---
+++ precache-manifest.494c547702845788d18f313fa06917fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d51d2173aed9dca793e677ca86ad634",
+    "url": "/react-turtle-game/index.html"
+  },
+  {
+    "revision": "235a39de20200f55ff93",
+    "url": "/react-turtle-game/static/css/main.b7c559e8.chunk.css"
+  },
+  {
+    "revision": "e924a2a2a9f78103021d",
+    "url": "/react-turtle-game/static/js/2.7287e7c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-turtle-game/static/js/2.7287e7c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "235a39de20200f55ff93",
+    "url": "/react-turtle-game/static/js/main.f5ce6086.chunk.js"
+  },
+  {
+    "revision": "a99d87cc6e222717480d",
+    "url": "/react-turtle-game/static/js/runtime-main.a2f9544a.js"
+  }
+]);
