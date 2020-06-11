adf8144f78aee67f8c5fba01cc2101be7167530c
MarcinOstaszewski <marcinostaszewski@gmail.com>
2020-06-11 13:43:41 +0200

    better Start and GameOver texts

--- src/components/GameOver/GameOver.js
+++ src/components/GameOver/GameOver.js
@@ -2,11 +2,11 @@ import React from 'react';
 import styles from './GameOver.module.css'
 
 const GameOver = (props) => {
-    console.log(props)
     return ( 
         <div id={styles.gameover}>
             <h2>GAME OVER</h2>
-            <p>Your score: {props.score}</p>
+            <h3>Your score:</h3>
+            <h1>{props.score}</h1>
             <div className={styles.btn}
                 onClick={ () => props.changeGameState('start') }
             >RESTART</div>
--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -8,15 +8,15 @@ const Start = (props) => {
             <div>Catch the circles for points.</div>
             <div>Brighter circles give more points.</div>
             <div>The greater is your's and circle's speed the more you score.</div>
-            <div>Collisions at high speed hurt you.</div>
-            <div>Red circles heal you.</div>
+            <div>Collisions with walls and obstacles hurt you.</div>
+            <div>Red circles heal you or give more points.</div>
             <div>Use <strong>w, s, a, d</strong> keys.</div>
             <div className={styles.btn}
                 onClick={ () => props.changeGameState('game') }
             >START</div>
 
         </div>
-     );
+    );
 }
  
 export default Start;
